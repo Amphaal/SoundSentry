97b531e2078e72886ae8da2262384912f6659ac9
Guillaume VARA <guillaume.vara@gmail.com>
2018-10-16 17:35:25 +0200

    dfdf

--- app.js
+++ app.js
@@ -12,36 +12,47 @@ io.on('connection', function(socket) {
     const userToWatch = socket.handshake.query.userToWatch;
     if (userToWatch) {
 
+        process.stdout.write("new connection for user's shouts \"" + userToWatch + "\"!");
+
         //create
         let pathToWatch = './users_data/' + userToWatch + "/shout.json";
 
-        //declare macro function for emit
-        let rfFunc = function () {
-            fs.readFile(pathToWatch, 'utf8', function (err, contents) {
-                socket.emit('newShout', contents);
-            }); 
-        };
-        rfFunc();
-
         //if no watcher registered
         if (!fileWatchers[userToWatch]) {
+            
             //watcher
-            let watch = fs.watch(pathToWatch, { encoding: 'buffer' }, () => {
-                rfFunc();
+            let watch = fs.watch(pathToWatch, { encoding: 'buffer' }, (eventType, filename) => {
+                fs.readFile(pathToWatch, 'utf8', function (err, contents) {
+                    io.of(socket.nsp.name).emit('newShout', contents);
+                }); 
             });
 
             //register...
+            process.stdout.write("registering \"" + userToWatch + "\" shouts watcher !");
             fileWatchers[userToWatch] = watch;
             if(socketsByFW[userToWatch] == undefined) socketsByFW[userToWatch] = 0;
-            socketsByFW[userToWatch] = socketsByFW[userToWatch] + 1;
         }
+
+        //initial shout fetch
+        fs.readFile(pathToWatch, 'utf8', function (err, contents) {
+            socket.emit('newShout', contents);
+        }); 
+        socketsByFW[userToWatch] = socketsByFW[userToWatch] + 1;
+
+        process.stdout.write("Succesful registering for user's shouts \"" + userToWatch + "\". Total number of connections for this user : " + socketsByFW[userToWatch]);
     }
 
     //on user disconnect
     socket.on('disconnect', function () {
-        //close watcher
+
         socketsByFW[userToWatch] = socketsByFW[userToWatch] - 1;
-        if(socketsByFW[userToWatch] <= 0) fileWatchers[userToWatch].close();
+        process.stdout.write("disconnect from user's shouts \"" + userToWatch + "\". Remaining connections for this user : " + socketsByFW[userToWatch]);
+
+        //close watcher
+        if(socketsByFW[userToWatch] <= 0) {
+            process.stdout.write("Closing \"" + userToWatch + "\" file watcher...");
+            fileWatchers[userToWatch].close(); 
+        }
     });
 });
 
