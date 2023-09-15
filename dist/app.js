3e35135716090e61a0bab99f14a6edf7ecbf2e32
Amphaal <guillaume.vara@gmail.com>
2023-09-15 05:27:54 +0200

    handle shutdown

--- dist/app.js
+++ dist/app.js
@@ -5,111 +5,148 @@ import { WebSocketServer } from 'ws';
 import { setupOnSocketReady as login_setupOnSocketReady } from './handlers/login.js';
 import { setupOnSocketReady as shout_setupOnSocketReady } from './handlers/shout.js';
 import { SoundVitrineDatabaseFolderPath, ListeningPort } from './_const.js';
+import ON_DEATH from 'death';
 
-//
-// Front Web Server setup
-//
-
-//
-if (!existsSync(SoundVitrineDatabaseFolderPath)) {
-  mkdirSync(SoundVitrineDatabaseFolderPath, { recursive: true });
-}
-
-var env = process.env.NODE_ENV || 'development';
-
-if (env != 'production') {
+async function main () {
   //
-  console.log("==> NON-PRODUCTION ENV, running non-secure HTTP server <==");
+  // Front Web Server setup
+  //
 
   //
-  var webServ = httpServer.createServer();
-} else {
-  //
-  console.log("==> PRODUCTION ENV, trying to run HTTPS server ... <==");
-
-  //
-  if (process.env.DOMAIN_NAME) {
-    console.error("[DOMAIN_NAME] env variable must be defined. It is used to determine certificate names from linked certbot SSL installation.");
+  if (!existsSync(SoundVitrineDatabaseFolderPath)) {
+    mkdirSync(SoundVitrineDatabaseFolderPath, { recursive: true });
   }
 
-  //
-  var webServ = httpsServer.createServer({
-    key: readFileSync(SoundVitrineDatabaseFolderPath + '/' + process.env.DOMAIN_NAME + '.key'),
-    cert: readFileSync(SoundVitrineDatabaseFolderPath + '/' + process.env.DOMAIN_NAME + '.crt')
-  });
-}
+  var env = process.env.NODE_ENV || 'development';
 
-//
-// link WSS with HTTP / HTTPS server
-//
-
-const wss = new WebSocketServer({ noServer: true });
-
-//
-function onSocketError() {
-  console.warn("Something wrong happened with WebSockets service");
-}
-
-//
-webServ.on('upgrade', function upgrade(request, socket, head) {
-  //
-  socket.on('error', onSocketError);
-
-  // url must be provided
-  if (request.url == null) {
-    socket.destroy();
-    return;
-  }
-
-  const [username, verb] = request.url.split('/').filter(Boolean);
-  
-  //get user to watch
-  if (username == null || verb == null) {
-    socket.destroy();
-    return;
-  }
-
-  //
-  wss.handleUpgrade(request, socket, head, function done(ws) {
+  if (env != 'production') {
     //
-    socket.removeListener('error', onSocketError);
+    console.log("==> NON-PRODUCTION ENV, running non-secure HTTP server <==");
 
-    // says it connected anyway...
-    wss.emit('connection', ws, request);
-    
     //
-    let middlewares = [];
-    switch (verb) {
-      case "shout": {
-        middlewares.push(shout_setupOnSocketReady(ws, wss.clients, username));
-      }
+    var webServ = httpServer.createServer();
+  } else {
+    //
+    console.log("==> PRODUCTION ENV, trying to run HTTPS server ... <==");
 
-      case "login": {
-        middlewares.push(login_setupOnSocketReady(ws, wss.clients, username));
-      }
+    //
+    if (process.env.DOMAIN_NAME) {
+      console.error("[DOMAIN_NAME] env variable must be defined. It is used to determine certificate names from linked certbot SSL installation.");
     }
 
     //
-    ws.on('message', function message(data) {
-      //
-      const payloadAsJson = JSON.parse(data);
+    var webServ = httpsServer.createServer({
+      key: readFileSync(SoundVitrineDatabaseFolderPath + '/' + process.env.DOMAIN_NAME + '.key'),
+      cert: readFileSync(SoundVitrineDatabaseFolderPath + '/' + process.env.DOMAIN_NAME + '.crt')
+    });
+  }
 
+  //
+  // link WSS with HTTP / HTTPS server
+  //
+
+  const wss = new WebSocketServer({ noServer: true });
+
+  //
+  function onSocketError() {
+    console.warn("Something wrong happened with WebSockets service");
+  }
+
+  //
+  webServ.on('upgrade', function upgrade(request, socket, head) {
+    //
+    socket.on('error', onSocketError);
+
+    // url must be provided
+    if (request.url == null) {
+      socket.destroy();
+      return;
+    }
+
+    const [username, verb] = request.url.split('/').filter(Boolean);
+    
+    //get user to watch
+    if (username == null || verb == null) {
+      socket.destroy();
+      return;
+    }
+
+    //
+    wss.handleUpgrade(request, socket, head, function done(ws) {
       //
-      for(const middleware of middlewares) {
-        const handled = middleware(payloadAsJson);
-        if (handled) return;
+      socket.removeListener('error', onSocketError);
+
+      // says it connected anyway...
+      wss.emit('connection', ws, request);
+      
+      //
+      let middlewares = [];
+      switch (verb) {
+        case "shout": {
+          middlewares.push(shout_setupOnSocketReady(ws, wss.clients, username));
+        }
+
+        case "login": {
+          middlewares.push(login_setupOnSocketReady(ws, wss.clients, username));
+        }
       }
 
       //
-      console.warn('Client trying to use unconfigured command, with payload: ', payloadAsJson);
+      ws.on('message', function message(data) {
+        //
+        const payloadAsJson = JSON.parse(data);
+
+        //
+        for(const middleware of middlewares) {
+          const handled = middleware(payloadAsJson);
+          if (handled) return;
+        }
+
+        //
+        console.warn('Client trying to use unconfigured command, with payload: ', payloadAsJson);
+      });
     });
   });
-});
 
-//
-// Igniting
-//
+  //
+  // Igniting
+  //
 
-webServ.listen(ListeningPort, '0.0.0.0');
+  let shutingDown = false;
 
-console.log("Sucessfully ran on port " + ListeningPort + ". Awaiting connections...");
+  //
+  const shutdownGracefully = () => {
+    //
+    if (shutingDown) return;
+
+    //
+    shutingDown = true;
+
+    //
+
+    //
+    console.log('[SoundSentry] Should shutdown now...');
+
+    //
+    webServ.on("close", () => {
+      console.log('[SoundSentry] Web server gracefully out. Bye-bye !')
+    });
+
+    webServ.close();
+  }
+
+  // if death-like signal intercepted at least once, try to close gracefully
+  ON_DEATH(shutdownGracefully)
+
+  webServ.listen(ListeningPort, '0.0.0.0');
+
+  console.log("Sucessfully ran on port " + ListeningPort + ". Awaiting connections...");
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main()
+  .catch((error) => {
+    console.error(error)
+    process.exit(1)
+  })
