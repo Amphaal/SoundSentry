a4f6547e86af63d12c06eba8aede81cc4dc3ace5
Amphaal <guillaume.vara@gmail.com>
2023-09-17 08:02:47 +0200

    ffs

--- dist/_const.js
+++ dist/_const.js
@@ -1,5 +1,5 @@
 export const SoundVitrineDatabaseFolderPath = process.env.DATA_FOLDER ?? "/srv/data";
-export const SSLCertFolderPath = "/etc/letsencrypt";
+export const SSLCertFolderPath = "/etc/letsencrypt/live";
 export const ListeningPort = 3000;
 export const ExpectedShoutFileNameOnUserProfile = "shout.json";
 export const ExpectedUserDatabaseFileName = "users.json";
